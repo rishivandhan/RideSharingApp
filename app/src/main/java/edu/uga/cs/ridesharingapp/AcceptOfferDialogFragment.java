d03281d8563cf40baeff63cca301ed937de896ee
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2025-04-26 18:25:07 -0400

    partially setup the AcceptOfferDialogFragment

--- app/src/main/java/edu/uga/cs/ridesharingapp/AcceptOfferDialogFragment.java
+++ app/src/main/java/edu/uga/cs/ridesharingapp/AcceptOfferDialogFragment.java
@@ -1,4 +1,36 @@
 package edu.uga.cs.ridesharingapp;
 
-public class AcceptOfferDialogFragment {
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+public class AcceptOfferDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+
+        builder.setTitle("Confirm");
+        builder.setMessage("Are you sure you want to accept this ride offer?");
+        builder.setPositiveButton("Confirm", new ConfirmClickListener());
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        return builder.create();
+    }
+
+    private class ConfirmClickListener implements DialogInterface.OnClickListener {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+        }
+    }
 }
