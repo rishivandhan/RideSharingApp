28930ca6c011a4b28bc86db22e9717372fae0782
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2025-04-26 22:34:06 -0400

    almost finished the main page list. Still need to add to user ride offer list and update both driver and rider accepted value to true, and set the currently logged in user as the rider id of the drive offer

--- app/src/main/java/edu/uga/cs/ridesharingapp/AcceptOfferDialogFragment.java
+++ app/src/main/java/edu/uga/cs/ridesharingapp/AcceptOfferDialogFragment.java
@@ -10,9 +10,34 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 public class AcceptOfferDialogFragment extends DialogFragment {
+    int position;
+    long date;
+    String key;
+    String startLocation;
+    String endLocation;
+
+    public static AcceptOfferDialogFragment newInstance (int position, String key, long date, String startLocation, String endLocation) {
+        AcceptOfferDialogFragment acceptOfferDialogFragment = new AcceptOfferDialogFragment();
+        Bundle args = new Bundle();
+
+        args.putInt("position", position);
+        args.putString("key", key);
+        args.putLong("date", date);
+        args.putString("startLocation", startLocation);
+        args.putString("endLocation", endLocation);
+
+        acceptOfferDialogFragment.setArguments(args);
+        return acceptOfferDialogFragment;
+    }
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        position = getArguments().getInt("position");
+        key = getArguments().getString("key");
+        date = getArguments().getLong("date");
+        startLocation = getArguments().getString("startLocation");
+        endLocation = getArguments().getString("endLocation");
+
         AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
 
         builder.setTitle("Confirm");
@@ -31,6 +56,16 @@ public class AcceptOfferDialogFragment extends DialogFragment {
     private class ConfirmClickListener implements DialogInterface.OnClickListener {
         @Override
         public void onClick(DialogInterface dialogInterface, int i) {
+            DriveOffer driveOffer = new DriveOffer(date, startLocation, endLocation);
+            driveOffer.setKey(key);
+            AcceptDriveOfferDialogListener acceptDriveOfferDialogListener = (AcceptDriveOfferDialogListener) requireActivity();
+            acceptDriveOfferDialogListener.acceptDriverOffer(position, driveOffer);
+
+            dismiss();
         }
+    }
+
+    public interface AcceptDriveOfferDialogListener {
+        void acceptDriverOffer (int position, DriveOffer driveOffer);
     }
 }
--- app/src/main/java/edu/uga/cs/ridesharingapp/RiderActivity.java
+++ app/src/main/java/edu/uga/cs/ridesharingapp/RiderActivity.java
@@ -11,21 +11,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 public class RiderActivity extends AppCompatActivity
-        implements CreateRequestDialogFragment.AddRideRequestDialogListener {
+        implements CreateRequestDialogFragment.AddRideRequestDialogListener,
+        AcceptOfferDialogFragment.AcceptDriveOfferDialogListener {
     private static final String DEBUG_TAG = "RiderActivity";
     private Button createRequestButton;
     private Button viewUnacceptedRequestsButton;
     private Button viewAcceptedRequestsButton;
     private RecyclerView availableOffersView;
     private String userID;
+    private List<DriveOffer> driveOffers = new ArrayList<>();
+    private AcceptDriveOfferAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +68,37 @@ public class RiderActivity extends AppCompatActivity
             unacceptedIntent.putExtras(uInfo);
             startActivity(unacceptedIntent);
         });
+
+        availableOffersView.setLayoutManager(new LinearLayoutManager(this));
+        loadAvailableOffers();
+    }
+
+    private void loadAvailableOffers () {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference offersRef = database.getReference("ride_offers");
+
+        offersRef.get().addOnSuccessListener(snapshot -> {
+            driveOffers = new ArrayList<>();
+
+            if (snapshot.exists()) {
+                for (DataSnapshot offerSnapshot : snapshot.getChildren()) {
+                    DriveOffer offer = offerSnapshot.getValue(DriveOffer.class);
+
+                    if (offer != null && !offer.isAccepted() && !offer.getDriverid().equals(userID)) {
+                        driveOffers.add(offer);
+                        Log.d(DEBUG_TAG, "Added offer: " + offer.getKey());
+                    }
+                }
+            }
+
+            adapter = new AcceptDriveOfferAdapter(driveOffers, RiderActivity.this);
+            availableOffersView.setAdapter(adapter);
+            Collections.sort(driveOffers, (offer1, offer2) -> Long.compare(offer1.getDate(), offer2.getDate()));
+            adapter.notifyDataSetChanged();
+        }).addOnFailureListener(e -> {
+            Log.e(DEBUG_TAG, "Failed to load drive offers", e);
+        });
+
     }
 
     @Override
@@ -90,6 +129,23 @@ public class RiderActivity extends AppCompatActivity
                 })
                 .addOnFailureListener(e -> {
                     Log.e(DEBUG_TAG, "Error storing in Firebase", e);
+                });
+    }
+
+    @Override
+    public void acceptDriverOffer(int position, DriveOffer driveOffer) {
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference offerRef = firebaseDatabase.getReference("ride_offers").child(driveOffer.getKey());
+
+        offerRef.child("accepted").setValue(true)
+                .addOnSuccessListener(aVoid -> {
+                    Toast.makeText(this, "Offer accepted successfully!", Toast.LENGTH_SHORT).show();
+                    driveOffers.remove(position);
+                    adapter.notifyItemRemoved(position);
+
+                })
+                .addOnFailureListener(e -> {
+                    Log.e(DEBUG_TAG, "Failed to accept offer", e);
                 });
     }
 }
