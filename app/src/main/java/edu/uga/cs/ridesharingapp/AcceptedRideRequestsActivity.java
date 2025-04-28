e1ee4c240150b21a863d7ad4db4299163f97ec21
ZephyrIO <66318348+ZephyrIO@users.noreply.github.com>
2025-04-27 21:49:50 -0400

    added basic implementation of double list of accepted rides from rider pov

--- app/src/main/java/edu/uga/cs/ridesharingapp/AcceptedRideRequestsActivity.java
+++ app/src/main/java/edu/uga/cs/ridesharingapp/AcceptedRideRequestsActivity.java
@@ -2,6 +2,7 @@ package edu.uga.cs.ridesharingapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,8 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
@@ -24,6 +27,8 @@ public class AcceptedRideRequestsActivity extends AppCompatActivity {
     private String userId;
     private List<RideRequest> acceptedRideRequests = new ArrayList<>();
     private List<DriveOffer> acceptedDriveOffers = new ArrayList<>();
+    private AcceptedRideRequestAdapter acceptedRideRequestAdapter;
+    private AcceptedDriveOfferAdapter acceptedDriveOfferAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +52,45 @@ public class AcceptedRideRequestsActivity extends AppCompatActivity {
         RiderAcceptedRequestsView.setLayoutManager(new LinearLayoutManager(this));
         RiderAcceptedOffersView.setLayoutManager(new LinearLayoutManager(this));
 
-        // TODO: create RecyclerViewAdapters
+        acceptedRideRequestAdapter = new AcceptedRideRequestAdapter(acceptedRideRequests, this);
+        acceptedDriveOfferAdapter = new AcceptedDriveOfferAdapter(acceptedDriveOffers, this);
+
+        RiderAcceptedRequestsView.setAdapter(acceptedRideRequestAdapter);
+        RiderAcceptedOffersView.setAdapter(acceptedDriveOfferAdapter);
+
+        loadAcceptedRideRequests();
+        loadAcceptedDriveOffers();
+    }
+
+    private void loadAcceptedRideRequests() {
+        DatabaseReference requestsRef = firebaseDatabase.getReference("ride_requests");
+
+        requestsRef.get().addOnSuccessListener(snapshot -> {
+            acceptedRideRequests.clear();
+            for (DataSnapshot rideSnapshot : snapshot.getChildren()) {
+                RideRequest rideRequest = rideSnapshot.getValue(RideRequest.class);
+
+                if (rideRequest != null && rideRequest.isAccepted() && userId.equals(rideRequest.getCreatorid())) {
+                    acceptedRideRequests.add(rideRequest);
+                }
+            }
+            acceptedRideRequestAdapter.notifyDataSetChanged();
+        }).addOnFailureListener(e -> Log.e(DEBUG_TAG, "Failed to load accepted ride requests", e));
+    }
+
+    private void loadAcceptedDriveOffers() {
+        DatabaseReference offersRef = firebaseDatabase.getReference("ride_offers");
+
+        offersRef.get().addOnSuccessListener(snapshot -> {
+            acceptedDriveOffers.clear();
+            for (DataSnapshot offerSnapshot : snapshot.getChildren()) {
+                DriveOffer driveOffer = offerSnapshot.getValue(DriveOffer.class);
+
+                if (driveOffer != null && driveOffer.isAccepted() && userId.equals(driveOffer.getRiderid())) {
+                    acceptedDriveOffers.add(driveOffer);
+                }
+            }
+            acceptedDriveOfferAdapter.notifyDataSetChanged();
+        }).addOnFailureListener(e -> Log.e(DEBUG_TAG, "Failed to load accepted drive offers", e));
     }
 }
